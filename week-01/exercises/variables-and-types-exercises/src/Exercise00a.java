3a332d3d04bb54750b5b4c7abbd03c1fa3d22547
Kevin Limlengco <limlengcokevin@gmail.com>
2020-12-08 11:32:47 -0600

    Update the Exercise 00a File

--- week-01/exercises/variables-and-types-exercises/src/Exercise00a.java
+++ week-01/exercises/variables-and-types-exercises/src/Exercise00a.java
@@ -1,9 +1,12 @@
 public class Exercise00a {
     public static void main(String[] args) {
         // 1. Add a single line comment below. Hint: this instruction is a single line comment.
-
+        // This is a single line comment
         /* 2. Add a multi-line comment below.
         Hint: this instruction is a multi-line comment.
+         */
+        /* This is a multi line comment
+        Hello
          */
     }
 }
