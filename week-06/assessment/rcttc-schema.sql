b8fedd2f5274dbeedafeeafc803538ee31bea533
Kevin Limlengco <limlengcokevin@gmail.com>
2021-01-23 17:51:44 -0600

    Finish DDL

--- week-06/assessment/rcttc-schema.sql
+++ week-06/assessment/rcttc-schema.sql
@@ -14,22 +14,22 @@ create table customer (
     address varchar(50) null
 );
 
-drop table if exists seats;
-create table seats (
-	seat_number char(5),
-    seat_id int primary key auto_increment);
-    
+drop table if exists theater;
+create table theater (
+	theater_id int primary key auto_increment,
+	theater_name varchar(25) not null,
+    theater_add varchar(50) not null,
+    theater_phone varchar(15) not null,
+    theater_email varchar(25) not null
+);
+
 drop table if exists show_times; 
 create table show_times (
 	showtime_id int primary key auto_increment,
     show_date date
 );
 
-drop table if exists shows;
-create table shows (
-	showid int not null primary key auto_increment,
-    show_name varchar(25) not null
-);
+
 
 drop table if exists times_show;
 create table times_show (
@@ -45,14 +45,35 @@ create table times_show (
 		primary key(showtime_id, showid)
 );
 
+
 drop table if exists ticket;
 create table ticket (
 	ticket_id int primary key auto_increment,
     tikcet_price decimal(3,2),
     customerid int not null,
+    showtime_id int not null,
     constraint fk_ticket_customerid
       foreign key (customerid)
       references customer(customerid),
     constraint fk_show_times
-	  foreign key (
+	  foreign key (showtime_id)
+      references show_times(showtime_id)
 );
+
+
+drop table if exists seats;
+create table seats (
+	seat_number char(5),
+    seat_id int primary key auto_increment,
+    theater_id int,
+    ticket_id int not null,
+    constraint fk_seats_theater_id
+		foreign key (theater_id)
+        references theater(theater_id),
+    constraint fk_seats_ticket_id
+		foreign key (ticket_id)
+        references ticket(ticket_id)
+    );
+    
+
+
