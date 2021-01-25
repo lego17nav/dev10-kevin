c88aa3092a6f0bf016a7b648c54298d66420500e
Kevin Limlengco <limlengcokevin@gmail.com>
2021-01-24 23:23:33 -0600

    query

--- week-06/assessment/rcttc-schema.sql
+++ week-06/assessment/rcttc-schema.sql
@@ -9,12 +9,10 @@ create table customer (
 	customerid int primary key auto_increment,
 	firstname varchar(25) not null,
     lastname varchar(25) not null,
-    email varchar(25) not null,
+    email varchar(50) not null,
     customer_phone varchar(15) null,
     address varchar(50) null
 );
-alter table customer
-	modify email varchar(50) not null;
 
 drop table if exists theater;
 create table theater (
@@ -25,13 +23,15 @@ create table theater (
     theater_email varchar(25) not null
 );
 
-drop table if exists show_times; 
-create table show_times (
-	showtime_id int primary key auto_increment,
-    show_date date
+drop table if exists shows;
+create table shows (
+	showid int not null primary key auto_increment,
+    show_name varchar(25) not null,
+    theater_id int not null,
+    constraint fk_shows_theater_id
+		foreign key (theater_id)
+        references theater(theater_id)
 );
-
-
 
 drop table if exists times_show;
 create table times_show (
@@ -46,36 +46,38 @@ create table times_show (
     constraint pk_times_show
 		primary key(showtime_id, showid)
 );
-
-
-drop table if exists ticket;
-create table ticket (
-	ticket_id int primary key auto_increment,
-    tikcet_price decimal(3,2),
-    customerid int not null,
-    showtime_id int not null,
-    constraint fk_ticket_customerid
-      foreign key (customerid)
-      references customer(customerid),
-    constraint fk_show_times
-	  foreign key (showtime_id)
-      references show_times(showtime_id)
-);
-
-
+  
 drop table if exists seats;
 create table seats (
 	seat_number char(5),
     seat_id int primary key auto_increment,
     theater_id int,
-    ticket_id int not null,
     constraint fk_seats_theater_id
 		foreign key (theater_id)
-        references theater(theater_id),
-    constraint fk_seats_ticket_id
-		foreign key (ticket_id)
-        references ticket(ticket_id)
-    );
+        references theater(theater_id)
+    );  
+
+drop table if exists ticket;
+create table ticket (
+	ticket_id int primary key auto_increment,
+    ticket_price decimal(5,2),
+    customerid int not null,
+    showtime_id int not null,
+    seat_id int not null,
+    constraint fk_ticket_customerid
+      foreign key (customerid)
+      references customer(customerid),
+    constraint fk_show_times
+	  foreign key (showtime_id)
+      references show_times(showtime_id),
+    constraint fk_ticket_seat_id
+      foreign key (seat_id)
+      references seats(seat_id)
+);
+
     
+
+
+
 
 
